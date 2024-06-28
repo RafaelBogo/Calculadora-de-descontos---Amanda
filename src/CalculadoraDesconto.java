b401ffaa94e3b53d89b27299ad615bf691f79526
Rafael Bogo <rafaelbogo52@gmail.com>
2024-06-28 11:57:25 -0300

    Nome da classe corrigido

--- src/CalculadoraDesconto.java
+++ src/CalculadoraDesconto.java
@@ -1,4 +1,4 @@
-public class CalcularDesconto {
+public class CalculadoraDesconto {
     public double calcular(Orcamento orcamento){
         Desconto desconto = new DescontoParaTresMeses(new DescontoPara3Alunos(new SemDesconto()));
         return desconto.calcular(orcamento);
--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         Orcamento o1 = new Orcamento(55, 3, 2);
-        CalcularDesconto calc = new CalcularDesconto();
+        CalculadoraDesconto calc = new CalculadoraDesconto();
         System.out.println(calc.calcular(o1));
     }
 }
